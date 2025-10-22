b8dbb1097be98816cdc8da162fd7c52d8a55d9e3
Romain Thomas <contact@romainthomas.net>
2025-10-22 14:57:22 +0200

    added missing Collaborations import in PubPart

--- dibisoplot/pubpart/__init__.py
+++ dibisoplot/pubpart/__init__.py
@@ -2,6 +2,7 @@ from dibisoplot.pubpart.pubpart import PubPart
 from dibisoplot.pubpart.pubpart import PubInstitutions
 from dibisoplot.pubpart.pubpart import InstitutionsLineageCollaborations
 from dibisoplot.pubpart.pubpart import PubTopics
+from dibisoplot.pubpart.pubpart import Collaborations
 from dibisoplot.pubpart.pubpart import TopicsCollaborations
 from dibisoplot.pubpart.pubpart import TopicsPotentialCollaborations
 from dibisoplot.pubpart.pubpart import WorksCollaborations
@@ -11,6 +12,7 @@ __all__ = [
     "PubInstitutions",
     "InstitutionsLineageCollaborations",
     "PubTopics",
+    "Collaborations",
     "TopicsCollaborations",
     "TopicsPotentialCollaborations",
     "WorksCollaborations",
